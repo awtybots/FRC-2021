edf5f6a07da2340c12ab4cf89ba832544cb73e9f
KashiPJ <79551401+KashiPJ@users.noreply.github.com>
2021-03-09 19:47:20 -0600

    added drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,8 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.DriveTrainSubsystem;
 
 public class Robot extends TimedRobot {
+
+  public static DriveTrainSubsystem drivetrainSubsystem = new DriveTrainSubsystem();
 
   @Override
   public void robotInit() {}
---
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+//yeetamouse
+//hi
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrainSubsystem extends SubsystemBase {
+  /** Creates a new DTsub. */
+
+  private TalonFX motorLeftBack = new TalonFX(1);
+  private TalonFX motorLeftFront = new TalonFX(2);
+  private TalonFX motorRightBack = new TalonFX(3);
+  private TalonFX motorRightFront = new TalonFX(4);
+
+  public DriveTrainSubsystem() {}
+
+  public void setMotorSpeeds(double leftSpeed, double rightSpeed) {
+    motorLeftBack.set(ControlMode.PercentOutput, leftSpeed);
+    motorLeftFront.set(ControlMode.PercentOutput, leftSpeed);
+
+    motorRightBack.set(ControlMode.PercentOutput, rightSpeed);
+    motorRightFront.set(ControlMode.PercentOutput, rightSpeed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
