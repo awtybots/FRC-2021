7e75b96917feb532da3c49914f3b17d4dd0ca1b4
matt-mekha <mtmekha8@gmail.com>
2021-03-23 19:26:58 -0500

    removed useless import

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -7,8 +7,6 @@
 
 package frc.robot.commands;
 
-import static frc.robot.Robot.*;
-
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
