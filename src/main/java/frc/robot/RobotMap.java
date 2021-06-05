61b025f9e02d356edf70baf92cb2c86076e0f168
matt-mekha <mtmekha8@gmail.com>
2021-06-05 15:35:18 -0500

    fixed some ids

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,24 +2,24 @@ package frc.robot;
 
 public abstract class RobotMap {
   public abstract static class CAN {
-    public static final int leftDrive1 = 1;
-    public static final int leftDrive2 = 2;
-    public static final int rightDrive1 = 3;
-    public static final int rightDrive2 = 4;
+    public static final int leftDrive1 = 13;
+    public static final int leftDrive2 = 14;
+    public static final int rightDrive1 = 11;
+    public static final int rightDrive2 = 12;
 
-    public static final int intake = 5;
-    public static final int spindexer = 6;
-    public static final int tower = 7;
+    public static final int intake = 5; // TODO
+    public static final int spindexer = 6; // TODO
+    public static final int tower = 7; // TODO
 
     public static final int turret = 8;
-    public static final int adjustableHood = 9;
-    public static final int shooter = 10;
+    public static final int adjustableHood = 9; // TODO
+    public static final int shooter = 15;
 
-    public static final int controlPanelSpinner = 13; // lmao
+    public static final int controlPanelSpinner = 10; // lmao
   }
 
   public abstract static class PCM {
-    public static final int intakeFwd = 0;
-    public static final int intakeRev = 4;
+    public static final int intakeFwd = 4;
+    public static final int intakeRev = 5;
   }
 }
