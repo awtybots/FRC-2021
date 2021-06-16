0bff82ee7a9376a75a8fae5feae8436af4879e17
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2021-06-16 13:53:34 -0500

    restore autoshoot we're using Manual Presets now

--- src/main/java/frc/robot/commands/teleop/automatic/AutoShoot.java
+++ src/main/java/frc/robot/commands/teleop/automatic/AutoShoot.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.AdjustableHoodSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TowerSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 import org.awtybots.frc.botplus.Logger;
 import org.awtybots.frc.botplus.math.Simulation;
 import org.awtybots.frc.botplus.math.Vector2;
@@ -25,6 +26,7 @@ public class AutoShoot extends CommandBase {
         ShooterSubsystem.getInstance(),
         TowerSubsystem.getInstance(),
         IndexerSubsystem.getInstance(),
+        TurretSubsystem.getInstance(),
         AdjustableHoodSubsystem.getInstance());
 
     SmartDashboard.putBoolean("Projectile Motion Solution", true);
@@ -60,50 +62,35 @@ public class AutoShoot extends CommandBase {
     }
 
     // TODO uncomment this once everything else is working
-    // Vector2 powerPortOffset = powerPort.getTargetDisplacement();
-    // SmartDashboard.putNumber("Power Port Perceived Distance",
-    // powerPortOffset.getX());
+    Vector2 powerPortOffset = powerPort.getTargetDisplacement();
+    SmartDashboard.putNumber("Power Port Perceived Distance", powerPortOffset.getX());
 
-    // double adjustableHoodGoalLaunchAngle =
-    // Math.toDegrees(Math.atan2(powerPortOffset.getX(), powerPortOffset.getY() +
-    // 1.0));
+    double adjustableHoodGoalLaunchAngle =
+        Math.toDegrees(Math.atan2(powerPortOffset.getX(), powerPortOffset.getY() + 1.0));
 
-    // AdjustableHoodSubsystem.getInstance().setGoalLaunchAngle(adjustableHoodGoalLaunchAngle);
+    AdjustableHoodSubsystem.getInstance().setGoalLaunchAngle(adjustableHoodGoalLaunchAngle);
 
     projectileMotionSimulation.setLaunchAngle(
         AdjustableHoodSubsystem.getInstance().getCurrentLaunchAngle());
-    // Vector2 velocity =
-    // projectileMotionSimulation.findOptimalLaunchVelocity(powerPortOffset);
-    Vector2 velocity = null; // TODO remove and uncomment lines above
+    Vector2 velocity = projectileMotionSimulation.findOptimalLaunchVelocity(powerPortOffset);
+    // Vector2 velocity = null; // TODO remove and uncomment lines above
     if (velocity == null) {
       SmartDashboard.putBoolean("Projectile Motion Solution", false);
-      logger.error("FAIL TO SHOOT"); // TODO this should do a default shot, not give up
-      // logger.warn(
-      // "Projectile motion simulation found no solution! Move the robot to a better
-      // shooting
-      // position.");
-      // return;
-      ShooterSubsystem.getInstance().setFlywheelRevsPerSecond(4400.0 / 60.0);
-      AdjustableHoodSubsystem.getInstance().setGoalLaunchAngle(55);
-    } else { // TODO cleanup
-      SmartDashboard.putBoolean("Projectile Motion Solution", true);
-      double goalRevsPerSecond =
-          ShooterSubsystem.getInstance().flywheel.ballVelocityToMotorRpm(velocity) / 60.0;
-      ShooterSubsystem.getInstance().setFlywheelRevsPerSecond(goalRevsPerSecond);
+      logger.warn(
+          "Projectile motion simulation found no solution! Move the robot to a better shooting position.");
+      return;
     }
+    SmartDashboard.putBoolean("Projectile Motion Solution", true);
+    double goalRevsPerSecond =
+        ShooterSubsystem.getInstance().flywheel.ballVelocityToMotorRpm(velocity) / 60.0;
+    ShooterSubsystem.getInstance().setFlywheelRevsPerSecond(goalRevsPerSecond);
 
     boolean readyToShoot =
-        AdjustableHoodSubsystem.getInstance()
-                .atGoalLaunchAngle() // TurretSubsystem.getInstance().atGoalAngle();
+        TurretSubsystem.getInstance().atGoalAngle()
+            && AdjustableHoodSubsystem.getInstance().atGoalLaunchAngle()
             && ShooterSubsystem.getInstance().isFlywheelReady();
 
     if (readyToShoot) {
-      // TODO remove soon
-      // try {
-      //   Thread.sleep(5000, 0);
-      // } catch (InterruptedException e) {
-      //   e.printStackTrace();
-      // }
       TowerSubsystem.getInstance().enableForShooting();
       IndexerSubsystem.getInstance().set(1);
     } else {
